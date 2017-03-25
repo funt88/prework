a34c100651ccefdcd4ae2a737254144fbd4bcd30
Marina <marina_kopell@yahoo.com>
2017-03-24 20:51:14 -0400

    Add files via upload

---
+++ Module-10/javascript.js
@@ -0,0 +1,29 @@
+ 
+
+        document.getElementById("blueBtn").addEventListener("click", function(){
+
+            document.getElementById("box").style.background = "blue";
+
+        });
+
+        document.getElementById("growBtn").addEventListener("click", function(){
+
+            document.getElementById("box").style.height = "250px";
+
+        });
+
+        document.getElementById("resetBtn").addEventListener("click", function(){
+
+            document.getElementById("box").style.height = "150px";
+            document.getElementById("box").style.background = "orange";
+            document.getElementById("box").style.transition = "opacity 2s";
+            document.getElementById("box").style.opacity = "1";
+        });
+
+        document.getElementById("fadeBtn").addEventListener("click", function(){
+
+            document.getElementById("box").style.transition = "opacity 2s";
+            document.getElementById("box").style.opacity = "0";
+         });
+
+    
